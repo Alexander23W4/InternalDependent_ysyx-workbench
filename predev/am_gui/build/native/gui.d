c97b0b3da518e0cbdd3475c58946a6080f3ae0ef
UB22.04Wang <2842wang6843w@gmail.com>
2026-01-30 10:26:27 +0800

    update 26-1-30

---
+++ predev/am_gui/build/native/gui.d
@@ -0,0 +1,6 @@
+/home/wang/My_ysyx-workbench/predev/am_gui/build/native/gui.o: \
+ /home/wang/My_ysyx-workbench/predev/am_gui/gui.c \
+ /home/wang/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/wang/ysyx-workbench/abstract-machine/am/include/arch/native.h \
+ /home/wang/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/wang/ysyx-workbench/abstract-machine/klib/include/klib-macros.h

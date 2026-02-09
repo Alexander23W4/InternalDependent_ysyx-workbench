a1d71692e4340edc9c5e70d015973b0a3ce61197
UB22.04Wang <2842wang6843w@gmail.com>
2026-02-09 18:00:01 +0800

    Mon Feb  9 18:00:01 CST 2026

--- VerTest/src/Cppcode/test2.cpp
+++ VerTest/src/Cppcode/test2.cpp
@@ -7,17 +7,24 @@
 #include "verilated.h"
 
 int main(int argc, char** argv) {
+    // verilator context pointer [STANDARD BUILD]
     VerilatedContext* p_context = new VerilatedContext;
     p_context->commandArgs(argc, argv);
+
+    // Object pointer [STANDARD BUILD]
     Vtest2* top = new Vtest2{p_context};
+
+    // Main loop [STANDARD BUILD]
     while (!p_context->gotFinish()) {
         int a = rand() & 1;
         int b = rand() & 1;
+
         top->a = a;
         top->b = b;
         top->eval();
         printf("a = %d, b = %d, c = %d\n", a, b, top->c);
-        assert(top->c == (a ^ b));
+
+        assert(top->c == (a ^ b));  //
     }
     delete top;
     delete p_context;

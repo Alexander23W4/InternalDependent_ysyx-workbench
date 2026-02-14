25f213ccf632d409f7bc1f081f1f0d517daf80e7
UB22.04Wang <2842wang6843w@gmail.com>
2026-02-14 09:55:28 +0800

    Sat Feb 14 09:55:28 CST 2026

--- VerTest/src/Cppcode/test2.cpp
+++ VerTest/src/Cppcode/test2.cpp
@@ -13,28 +13,39 @@ int main(int argc, char** argv) {
     //
     VerilatedContext* p_context = new VerilatedContext;
     p_context->commandArgs(argc, argv);
-    //
     Vtest2* top = new Vtest2{p_context};
-    //
+
+    // 1. Enable tracing
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
-    top->trace(tfp, 99);  // Trace 99 levels of hierarchy 
+    top->trace(tfp, 9); 
+    // open the output vcd file for writing the waveform data
+    // could be customized
+    tfp->open("test2.vcd"); // Make sure this directory exists!
 
-    tfp->open("obj_dir/t_trace_ena_cc/simx.vcd");
+    // 2. Define simulation limit
+    const vluint64_t max_sim_time = 100;
 
 
-    while (!p_context->gotFinish()) {
+    while (!p_context->gotFinish() && p_context->time() < max_sim_time) {
+        p_context->timeInc(1)
         int a = rand() & 1;
         int b = rand() & 1;
 
         top->a = a;
         top->b = b;
         top->eval();
-        printf("a = %d, b = %d, c = %d\n", a, b, top->c);
 
+        // 3. Dump data AFTER eval so the VCD catches the new values
+        tfp->dump(p_context->time());
+
+        printf("Time %ld: a = %d, b = %d, c = %d\n", p_context->time(), a, b, top->c);
+
+        // Verification logic
         assert(top->c == (a ^ b));
     }
     
+    tfp->close();
     delete top;
     delete p_context;
     return 0;

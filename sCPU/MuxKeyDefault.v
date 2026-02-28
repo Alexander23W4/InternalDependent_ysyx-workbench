033651e3ce95e8ef8db8e8fc6e981bd71647cb84
UB22.04Wang <2842wang6843w@gmail.com>
2026-02-28 13:54:40 +0800

    Sat Feb 28 13:54:40 CST 2026

--- sCPU/MuxKeyDefault.v
+++ sCPU/MuxKeyDefault.v
@@ -1,1 +1,19 @@
-module 
+module MuxKeyDefault #(
+    parameter AM = 2,
+    parameter KEY_W = 2,
+    parameter VALUE_W = 5,
+)
+(
+    input [KEY_W - 1: 0] key,
+    input [AM * (KEY_W + VALUE_W) - 1: 0] data,
+    input [VALUE_W - 1: 0] default_out;
+    output reg [VALUE_W - 1: 0] out
+)
+    MuxKeyInternal #(AM, KEY_W, VALUE_W, 1) Mux1 (
+        .key(key),
+        .data(data),
+        .default_out(default_out),
+        .out(out)
+    );
+
+endmodule

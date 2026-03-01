1ce8d65b17b012f5477c3f0fe8843b03636924b4
UB22.04Wang <2842wang6843w@gmail.com>
2026-03-01 21:40:58 +0800

    Sun Mar  1 21:40:58 CST 2026

--- sCPU/Tests/T_AS.cpp
+++ sCPU/Tests/T_AS.cpp
@@ -1,4 +1,4 @@
-#include "Vtest1.h"
+#include "Vtop.h"
 #include "verilated.h"
 
 #include <stdio.h>
@@ -12,14 +12,13 @@ int main(int argc, char** argv) {
 
     Vtop* top = new Vtop{contextp};   // modification
 
-    for (size_t i = 0; i < 100; i++)    
+    for (size_t index = 0; index < 100; index++)    
     {
         /* code */
-        p_contect->timeInc(1);
+        contextp->timeInc(1);
         int i = rand() % 9;
         int j = rand() % 9;
         
-        top->N = 4;
         top->a = ARR[i];    // signed -> binary  automatically?
         top->b = ARR[j];
         top->cin = rand() & 1;
@@ -27,8 +26,8 @@ int main(int argc, char** argv) {
         top->eval(); 
 
         printf("Time %ld: a = %d, b = %d, cin = %d, output = %d, \
-            overflow = %d, carry = %d\n", p_context->time(), top->a, \
-            top->b, top->cin, top->s, top->overflow, top->carry);
+            overflow = %d, carry = %d\n", contextp->time(), top->a, \
+            top->b, top->cin, top->s, top->overflow, top->c);
     }
     
 

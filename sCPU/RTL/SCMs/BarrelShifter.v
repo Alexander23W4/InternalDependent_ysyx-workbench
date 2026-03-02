698c16d91934bb355da5d148d81b942b307b8ee5
UB22.04Wang <2842wang6843w@gmail.com>
2026-03-02 20:27:54 +0800

    Mon Mar  2 20:27:54 CST 2026

---
+++ sCPU/RTL/SCMs/BarrelShifter.v
@@ -0,0 +1,29 @@
+module BarrelShifter #(parameter N = 3) (
+    input LR,
+    input AL,
+    input clk,
+    input rst,
+    input [2*N - 1: 0] din,
+    input [N - 1: 0] shamt,
+    output reg [2*N - 1: 0] dout
+);
+    // wire [2*N - 1: 0] _shift_remain;
+    // assign _shift_remain = |shamt ? {dout[2*N - 1: 2*N - shamt], (2*N - shamt){1'b0}} : dout;
+    always @(posedge clk) begin
+        if(rst) begin
+            dout <= din;
+        end
+        else begin
+            case(LR)
+                1'b1: begin
+                    dout <= (dout << shamt);
+                end
+                1'b0: begin
+                    dout <= AL ? (dout >>> shamt) : (dout >> shamt);
+                end
+            endcase            
+        end
+
+    end
+
+endmodule

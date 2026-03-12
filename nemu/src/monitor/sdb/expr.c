2547f71e70bd086ed402fe61314ef236113bf2e9
UB22.04Wang <2842wang6843w@gmail.com>
2026-03-12 11:52:39 +0800

    Thu Mar 12 11:52:39 CST 2026

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -83,7 +83,14 @@ typedef struct token {    // token definition
 static Token tokens[32] __attribute__((used)) = {};    // tokens arr
 static int nr_token __attribute__((used))  = 0;       // amount of tokens
 
-
+static void print_tokens__dbg(){
+  printf("DEBUG: PRINT TOKEN\n");
+  for (int i = 0; i < nr_token; i++)
+  {
+    printf("%d  ", tokens[i].type);
+    printf("%s\n", tokens[i].str);
+  }
+}
 
 
 static bool make_token(char *e) {   // !!! 
@@ -153,7 +160,7 @@ static bool make_token(char *e) {   // !!!
         break;
       }
     }
-
+    print_tokens__dbg();
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
@@ -163,6 +170,55 @@ static bool make_token(char *e) {   // !!!
   return true;
 }
 
+
+/*
+-> A token that is not an operator is not a main operator.
+-> A token that appears in a pair of parentheses is not a principal operator. 
+  Notice that there are no parentheses enclosing the entire expression,
+  because this is handled in the corresponding if block of check_parentheses().
+-> The main operator has the lowest precedence in the expression. 
+  This is because the main operator is the last operator to be performed.
+-> When more than one operator has the lowest priority, the last operator to be combined is the main operator by combinability. 
+  An example would be 1 + 2 + 3, whose main operator would be + on the right.
+*/
+ // inclusively tackle the tokens
+uint32_t eval(p, q) {
+  if (p > q) {
+    printf("p can not be larger than q.\n");
+    return -1;
+  }
+  else if (p == q) {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+    Assert((tokens[p].type == TK_HEX ) || (tokens[p].type == TK_NUM), "inclusive syntax error, &2");
+    uint32_t ret;
+    char* endptr;
+    if(tokens[p].type == TK_HEX) {ret = (uint32_t)strtol(tokens[p].str, &endptr, 16);}
+    else {ret = atoi(tokens[p].str);}
+    return ret;
+  }
+  else if (check_parentheses(p, q) == true) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1);
+  }
+  else {
+    op = the position of main operator in the token expression;
+    val1 = eval(p, op - 1);
+    val2 = eval(op + 1, q);
+
+    switch (op_type) {
+      case '+': return val1 + val2;
+      case '-': /* ... */
+      case '*': /* ... */
+      case '/': /* ... */
+      default: assert(0);
+    }
+  }
+}
 
 word_t expr(char *e, bool *success) {   // !!!
   if (!make_token(e)) {

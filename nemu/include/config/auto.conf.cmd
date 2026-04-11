2e824d2ada7d2891a36a7ae28ce69075c404f3e1
UB22.04Wang <2842wang6843w@gmail.com>
2026-04-11 11:01:12 +0800

    Sat Apr 11 11:01:12 CST 2026

--- nemu/include/config/auto.conf.cmd
+++ nemu/include/config/auto.conf.cmd
@@ -2,7 +2,7 @@ deps_config := \
 	src/device/Kconfig \
 	src/memory/Kconfig \
 	src/isa/riscv32/Kconfig \
-	/home/wang/My_ysyx-workbench/nemu/Kconfig
+	/home/wang/InternalDependent_ysyx-workbench/nemu/Kconfig
 
 include/config/auto.conf: \
 	$(deps_config)

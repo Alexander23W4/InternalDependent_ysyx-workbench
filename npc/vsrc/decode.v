5526d810433290c5e923dca8ee44a9c01191ff63
UB22.04Wang <2842wang6843w@gmail.com>
2026-04-11 22:53:48 +0800

    Sat Apr 11 22:53:48 CST 2026

--- npc/vsrc/decode.v
+++ npc/vsrc/decode.v
@@ -32,19 +32,77 @@ INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(
 */
 module decode(
     input [31:0] instr,
-    output [2:0] funct3,
-    output addi,
-    output add,
-    output jalr,
-    output lui,
-    output lw,
-    output lbu,
-    output sw,
-    output sb,
-    output ebreak,
+
+    output reg addi,
+    output reg add,
+    output reg jalr,
+    output reg lui,
+    output reg lw,
+    output reg lbu,
+    output reg sw,
+    output reg sb,
+    output reg ebreak,
+
+    output [4:0] rd,
+    output [4:0] rs1,
+    output [4:0] rs2,
+
+    output [31:0] immI,
+    output [31:0] immU,
+    output [31:0] immS
 );
-    wire [6:0] opcode;
+    wire [6:0] opcode = instr[6:0];
+    wire [2:0] funct3 = instr[14:12];
     
+    assign rd = instr[11:7];
+    assign rs1 = instr[19:15];
+    assign rs2 = instr[24:20];
+
+    assign immI = {{20{instr[31]}}, instr[31:20]};
+    assign immU = {instr[31:12], {12{1'b0}}};
+    assign immS = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+
+    always @(*) begin
+        addi = 0;
+        add = 0;
+        jalr = 0;
+        lui = 0;
+        lw = 0;
+        lbu = 0;
+        sw = 0;
+        sb = 0;
+        ebreak = 0;
+
+        case(opcode)
+            7'b0110011: begin
+                add = 1'b1;
+            end
+            7'b0010011: begin
+                addi = 1'b1;
+            end
+            7'b1100111: begin
+                jalr = 1'b1;
+            end
+            7'b0110111: begin
+                lui = 1'b1;
+            end
+            7'b0000011: begin
+                case(funct3) 
+                    3'b010: lw = 1'b1;
+                    3'b100: lbu = 1'b1;
+                endcase
+            end
+            7'b0100011: begin
+                case(funct3)
+                    3'b010: sw = 1'b1;
+                    3'b000: sb = 1'b1;
+                endcase
+            end
+            7'b1110011: begin
+                ebreak = 1'b1;
+            end
+        endcase 
+    end
 
 endmodule
 

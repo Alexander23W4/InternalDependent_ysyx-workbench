5d2888dc2a829f11bf8af1596882424457f229c8
UB22.04Wang <2842wang6843w@gmail.com>
2026-04-30 16:36:57 +0800

    Thu Apr 30 16:36:57 CST 2026

---
+++ npc/dpi_c_test/test1/obj_dir/main.d
@@ -0,0 +1,6 @@
+main.o: ../main.cpp /usr/local/share/verilator/include/vltstd/svdpi.h \
+ Vour__Dpi.h /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h Vour.h
---
+++ npc/tests/obj_dir/test_topl.d
@@ -0,0 +1,8 @@
+test_topl.o: ../test_c/test_topl.cpp \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h \
+ /usr/local/share/verilator/include/vltstd/svdpi.h Vtopl.h Vtopl__Dpi.h \
+ ../test_c/dbg.h

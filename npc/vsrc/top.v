393e762be981c0eb7f4f90b2ceeb35ece933b642
UB22.04Wang <2842wang6843w@gmail.com>
2026-05-05 15:27:29 +0800

    Tue May  5 15:27:29 CST 2026

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -176,18 +176,6 @@ decode Decode(
     );
 
 /*
-// ram access(load)
-INSTPAT("??????? ????? ????? 001 ????? 00000 11", lh     , I, R(rd) = SEXT(Mr(src1 + imm, 2), 16));
-INSTPAT("??????? ????? ????? 000 ????? 00000 11", lb     , I, R(rd) = SEXT(Mr(src1 + imm, 1), 8));
-
-// ram write
-INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2)); 
-INSTPAT("??????? ????? ????? 001 ????? 01000 11", sh     , S, Mw(src1 + imm, 2, src2)); // @ 
-INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2)); 
-
-// direct jump
-INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(rd) = s->snpc, s->dnpc = (src1 + imm) & ~1);  // jump and link reg
-INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->snpc; s->dnpc = s->pc + imm); 
 
 // conditional branch (if + jump)
 INSTPAT("??????? ????? ????? 100 ????? 11000 11", blt    , B, s->dnpc = ((int32_t)src1 < (int32_t)src2) ? s->pc + imm : s->snpc);  // branch if less than
@@ -196,35 +184,6 @@ INSTPAT("??????? ????? ????? 101 ????? 11000 11", bge    , B, s->dnpc = ((int32_
 INSTPAT("??????? ????? ????? 111 ????? 11000 11", bgeu   , B, s->dnpc = (src1 >= src2) ? s->pc + imm : s->snpc);
 INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne    , B, s->dnpc = (src1 != src2) ? s->pc + imm : s->snpc);
 INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, s->dnpc = (src1 == src2) ? s->pc + imm : s->snpc);
-
-// if
-INSTPAT("0000000 ????? ????? 010 ????? 01100 11", slt    , R, R(rd) = ((int32_t)src1 < (int32_t)src2) ? 1 : 0);
-INSTPAT("0000000 ????? ????? 011 ????? 01100 11", sltu   , R, R(rd) = ((uint32_t)src1 < (uint32_t)src2) ? 1 : 0); 
-INSTPAT("??????? ????? ????? 010 ????? 00100 11", slti   , I, R(rd) = ((int32_t)src1 < (int32_t)imm) ? 1 : 0); 
-INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu  , I, R(rd) = ((uint32_t)src1 < (uint32_t)imm) ? 1 : 0); 
-
-// shift
-INSTPAT("0000000 ????? ????? 001 ????? 00100 11", slli   , I, R(rd) = src1 << (uint32_t)(imm & 0x1F));
-INSTPAT("0000000 ????? ????? 101 ????? 00100 11", srli   , I, R(rd) = src1 >> (uint32_t)(imm & 0x1F));
-INSTPAT("0100000 ????? ????? 101 ????? 00100 11", srai   , I, R(rd) = ((int32_t)src1) >> (uint32_t)(imm & 0x1F)); // @
-INSTPAT("0000000 ????? ????? 001 ????? 01100 11", sll    , R, R(rd) = src1 << (uint32_t)(src2 & 0x1F));    // shifter left logical
-INSTPAT("0000000 ????? ????? 101 ????? 01100 11", srl    , R, R(rd) = src1 >> (uint32_t)(src2 & 0x1F));
-INSTPAT("0100000 ????? ????? 101 ????? 01100 11", sra    , R, R(rd) = ((int32_t)src1) >> (uint32_t)(src2 & 0x1F));   // shifte right arithematic  
-
-// logic
-INSTPAT("0000000 ????? ????? 111 ????? 01100 11", and    , R, R(rd) = src1 & src2); // @
-INSTPAT("??????? ????? ????? 111 ????? 00100 11", andi   , I, R(rd) = src1 & imm); // @
-INSTPAT("0000000 ????? ????? 110 ????? 01100 11", or     , R, R(rd) = src1 | src2); // @
-INSTPAT("??????? ????? ????? 110 ????? 00100 11", ori    , I, R(rd) = src1 | imm); // @
-INSTPAT("0000000 ????? ????? 100 ????? 01100 11", xor    , R, R(rd) = src1 ^ src2); // @
-INSTPAT("??????? ????? ????? 100 ????? 00100 11", xori   , I, R(rd) = src1 ^ imm); // @
-
-// alu
-INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , R, R(rd) = src1 + src2); // @
-INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
-INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(rd) = src1 - src2); // @
-
-INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0, NEMU_END
 */
 
 
@@ -235,25 +194,70 @@ INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(
                  jal | jalr |
                  lb | lh | lw | lbu | lhu;
 
-    wire [31:0] add1 = ({32{auipc}} & pc) |
+
+    wire [31:0] add1 = ({32{auipc | jal}} & pc) |
                         rdata1;
 
     wire [31:0] add2 = ({32{add}} & rdata2) |
-                       ({32{sw | sb}} & immS) |
+                       ({32{sw | sb | sh}} & immS) |
                        ({32{auipc}} & immU) |
-                       ({32{lbu | lw | lhu}} & immI) |
+                       ({32{jal}} & immJ) |
+                       ({32{lbu | lw | lhu | lh | lb | addi}} & immI) |
                        
-
 
     wire [31:0] add_rst = add1 + add2;
 
 
+    // other arithm/logic
+    wire[31:0] sub_rst = rdata1 - rdata2;
+    wire[31:0] xor_rst = rdata1 ^ rdata2;
+    wire[31:0] xori_rst = rdata1 ^ immI;
+    wire[31:0] or_rst = rdata1 | rdata2;
+    wire[31:0] ori_rst = rdata1 | immI;
+    wire[31:0] and_rst = rdata1 & rdata2;
+    wire[31:0] andi_rst = rdata1 & immI;
+
+    wire[31:0] slt_rst  = {31'b0, ($signed(rdata1) < $signed(rdata2))};
+    wire[31:0] sltu_rst = {31'b0, (rdata1 < rdata2)};
+    wire[31:0] slti_rst = {31'b0, ($signed(rdata1) < $signed(immI))};
+    wire[31:0] sltiu_rst= {31'b0, (rdata1 < immI)};
+
+    wire[4:0]  shamt_r  = rdata2[4:0];
+    wire[4:0]  shamt_i  = immI[4:0];
+    wire[31:0] sll_rst  = rdata1 << shamt_r;
+    wire[31:0] slli_rst = rdata1 << shamt_i;
+    wire[31:0] srl_rst  = rdata1 >> shamt_r;
+    wire[31:0] srli_rst = rdata1 >> shamt_i;
+    wire[31:0] sra_rst  = $signed(rdata1) >>> shamt_r;
+    wire[31:0] srai_rst = $signed(rdata1) >>> shamt_i;
+
+
+
     assign wdata = ({32{lui}} & immU) | 
                    ({32{add | addi | auipc}} & add_rst) | 
-                   ({32{jalr}} & pc_next_dft) |
+                   ({32{jalr | jal}} & pc_next_dft) |
                    ({32{lw}} & lw_rst) |
                    ({32{lbu}} & lbu_rst) |
-                   ({32{lhu}} & lhu_rst)
+                   ({32{lhu}} & lhu_rst) |
+                   ({32{lb}} & lb_rst) |
+                   ({32{lh}} & lh_rst) |     
+                   ({32{sub}} & sub_rst) |
+                   ({32{xor_inst}} & xor_rst) |
+                   ({32{xori}} & xori_rst) |
+                   ({32{or_inst}} & or_rst) |
+                   ({32{ori}} & ori_rst) |
+                   ({32{and_inst}} & and_rst) |
+                   ({32{andi}} & andi_rst) |
+                   ({32{sll}} & sll_rst) |
+                   ({32{slli}} & slli_rst) |
+                   ({32{srl}} & srl_rst) |
+                   ({32{srli}} & srli_rst) |
+                   ({32{sra}} & sra_rst) |
+                   ({32{srai}} & srai_rst) |
+                   ({32{slt}} & slt_rst) |
+                   ({32{slti}} & slti_rst) |
+                   ({32{sltu}} & sltu_rst) |
+                   ({32{sltiu}} & sltiu_rst);   
 
     // read ram
     wire [31:0] lw_rst;
@@ -274,7 +278,7 @@ INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(
             pc <= 32'h80000000;
         end
         else begin
-            pc <= jalr ? (add_rst & ~32'h1) : pc_next_dft;  // update
+            pc <= jalr ? (add_rst & ~32'h1) : jal? add_rst : pc_next_dft;  // update
 
             // write ram
             if(sw) begin
@@ -282,6 +286,9 @@ INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(
             end
             else if(sb) begin
                 ram_write(add_rst, rdata2, 1);
+            end
+            else if(sh) begin
+                ram_write(add_rst, rdata2, 2);
             end
         end
     end

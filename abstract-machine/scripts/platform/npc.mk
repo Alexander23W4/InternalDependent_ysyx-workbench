98f87abace6c09a31879e5010b3f28d71a151538
UB22.04Wang <2842wang6843w@gmail.com>
2026-05-07 18:13:57 +0800

    Thu May  7 18:13:57 CST 2026

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -17,13 +17,8 @@ MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
 CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINARGS_PLACEHOLDER)
 
-
--include $(NPC_HOME)/Makefile
-
-NPC_EXE=$(NPC_HOME)/obj_dir/Vtop
 EXE_ARGV=./build/$(ALL)-$(ARCH).bin
-
-ARGS=$(EXE_ARGV) -diff=$(DIFF_REF_SO)
+-include $(NPC_HOME)/Makefile
 
 update-npc:
 	@echo "Update NPC"

ed7b2ec82aa93c637c2e354fd39c1d73c2aed7fe
UB22.04Wang <2842wang6843w@gmail.com>
2026-05-08 09:19:53 +0800

    Fri May  8 09:19:53 CST 2026 rv32e-npc passes cpu-test

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -17,7 +17,6 @@ MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
 CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINARGS_PLACEHOLDER)
 
-EXE_ARGV=./build/$(ALL)-$(ARCH).bin
 -include $(NPC_HOME)/Makefile
 
 update-npc:
--- nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/ref.d
+++ nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/ref.d
@@ -3,13 +3,13 @@ cmd_/home/wang/InternalDependent_ysyx-workbench/nemu/build/obj-riscv32-nemu-inte
 source_/home/wang/InternalDependent_ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/ref.o := src/cpu/difftest/ref.c
 
 deps_/home/wang/InternalDependent_ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/ref.o := \
+    $(wildcard include/config/mbase.h) \
   /home/wang/InternalDependent_ysyx-workbench/nemu/include/isa.h \
   /home/wang/InternalDependent_ysyx-workbench/nemu/src/isa/riscv32/include/isa-def.h \
     $(wildcard include/config/rve.h) \
     $(wildcard include/config/rv64.h) \
   /home/wang/InternalDependent_ysyx-workbench/nemu/include/common.h \
     $(wildcard include/config/target/am.h) \
-    $(wildcard include/config/mbase.h) \
     $(wildcard include/config/msize.h) \
     $(wildcard include/config/isa64.h) \
   /home/wang/InternalDependent_ysyx-workbench/nemu/include/macro.h \

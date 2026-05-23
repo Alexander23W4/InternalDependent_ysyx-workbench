bc042c824ccad0cff243f51a230e4076699799a1
UB22.04Wang <2842wang6843w@gmail.com>
2026-05-23 15:31:44 +0800

    Sat May 23 15:31:44 CST 2026

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -36,7 +36,9 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     // !!! only deal with ev.event until now
     switch (c->mcause) {
-      case 0xb: ev.event = EVENT_YIELD; break;
+      case 0xb: ev.event = EVENT_YIELD;
+        c->mepc += 4;
+        break;
       default: ev.event = EVENT_ERROR; break;
     }
     // print_context(c);

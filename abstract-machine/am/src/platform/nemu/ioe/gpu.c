5bd652b61076526b877e928886e1d785d10c5c13
UB22.04Wang <2842wang6843w@gmail.com>
2026-08-28 10:11:58 +0800

    Fri Aug 28 10:11:58 CST 2026

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -12,7 +12,17 @@
 2. AM_GPU_FBDRAW, AM frame buffer controller, can write drawing information, drawing a w*h rectangular image at the screen coordinates (x, y). 
     The image pixels are stored in pixels in row-major order, with each pixel described as a 32-bit integer in the 00RRGGBB format for color. 
     If sync is true, the contents of the frame buffer will be immediately synchronized to the screen.
+
+⭐ 这些就是相当于硬件API, 裸机嵌入式编程的库函数, 这些共同构成IOE  (void __am_gpu_config(AM_GPU_CONFIG_T *cfg) 和裸机标准库函数的结构一模一样)
+  这里的"gpu"不是现在普遍标准的硬件接口, 正常来说, 标准硬件接口如USB, USART的基本API代码是包含在Linux内核里面的, Linux在扫描 设备树之后, 对其做完善
+
+  USB：drivers/usb/ 目录下包含主机控制器驱动（如 xHCI）、设备驱动框架（如 HID、存储、网络等）。
+  UART：drivers/tty/serial/ 目录下包含 8250、amba-pl011、omap 等具体控制器的驱动。
+
+  对于裸机嵌入式来说, 下载标准库函数, 就有了IOE, 加上RTOS, 就有了CTE
 */
+
+
 void __am_gpu_init() {
   // int i;
   // uint32_t vga_info = inl(VGACTL_ADDR);

f86a345f942a49ffd81998b1737537e0a2f4adaf
UB22.04Wang <2842wang6843w@gmail.com>
2026-09-03 21:25:30 +0800

    Thu Sep  3 21:25:30 CST 2026 complete the most basic AXI4_Lite, not includes strb resp

--- VerTest/bus/lsu_axi.sv
+++ VerTest/bus/lsu_axi.sv
@@ -57,3 +57,238 @@ ID 标签	                 AWID, ARID, WID, RID, BID	                           
 
 
 */
+
+interface AXI4_Lite;
+    logic [31:0] araddr;
+    logic        arvalid;
+    logic        arready;
+
+    logic [31:0] rdata;
+    logic [1:0]  rresp;
+    logic        rvalid;
+    logic        rready;
+
+    logic [31:0] awaddr;
+    logic        awvalid;
+    logic        awready;
+
+    logic [31:0] wdata;
+    logic [3:0]  wstrb;
+    logic        wvalid;
+    logic        wready;
+
+    logic [1:0]  bresp;
+    logic        bvalid;
+    logic        bready;
+
+    modport master (
+        output araddr, arvalid,
+        input  arready,
+
+        input  rdata, rresp, rvalid,
+        output rready,
+
+        output awaddr, awvalid,
+        input  awready,
+
+        output wdata, wstrb, wvalid,
+        input  wready,
+
+        input  bresp, bvalid,
+        output bready
+    );
+
+    modport slave (
+        input  araddr, arvalid,
+        output arready,
+
+        output rdata, rresp, rvalid,
+        input  rready,
+
+        input  awaddr, awvalid,
+        output awready,
+
+        input  wdata, wstrb, wvalid,
+        output wready,
+        
+        output bresp, bvalid,
+        input  bready
+    );
+
+
+    
+endinterface //AXI4_Lite
+
+
+/*
+    modport master (
+        output araddr, arvalid,
+        input  arready,
+
+        input  rdata, rvalid,       rresp
+        output rready,
+
+        output awaddr, awvalid,
+        input  awready,
+
+        output wdata, wvalid,       wstrb
+        input  wready,
+
+        input  bvalid,              bresp
+        output bready
+    );
+*/
+
+module LSU (
+    AXI4_Lite.master bus,
+    
+    input clk,
+    input reset,
+
+    input sw, sb, sh, lw, lb, lbu, lhu, lh,
+    // 这两个是只有在pc更新之后才清0, 完成之后一直为1
+    input decode_addr_ready,  
+    input decode_data_ready,
+
+    input [31:0] addr,
+    input [31:0] wdata,
+    input finish_arrange,   // 代表处理器后续部分处理完毕
+    
+    output [31:0] rdata
+);
+// 外部控制信号与返回外部的信号:
+    logic read, write;
+    logic [31:0] rdata_save;
+
+    assign read = lw | lb | lbu | lhu | lh;
+    assign write = sw | sb | sh;
+    assign rdata = rdata_save;
+
+    typedef enum [2:0]{ 
+        IDLE, AR, R, AW, W, B
+    } state_t;
+    state_t state, next;
+
+    always_ff @( posedge clk or posedge reset ) begin
+        if(reset) begin
+            state <= IDLE;
+            rdata_save <= `0;
+        end else begin
+            if(state == R && bus.rvalid == 1'b1) begin
+                rdata_save <= bus.rdata;
+            end
+            state <= next;
+        end
+        
+    end
+
+    always_comb begin
+        bus.araddr = addr;
+        bus.arvalid = 1'b0;
+        bus.rready = 1'b0;
+        bus.awaddr = addr;
+        bus.awvalid = 1'b0;
+        bus.wdata = wdata;
+        bus.wvalid = 1'b0;
+        bus.bready = 1'b0;
+        next = state;
+        
+        case(state)
+            IDLE: begin
+                if(decode_addr_ready && read) begin
+                    next = AR;
+                end
+                else if(decode_addr_ready && write) begin
+                    next = AW;
+                end
+            end
+
+            AR: begin
+                bus.arvalid = 1'b1;
+                if(bus.arready == 1'b1) begin
+                    next = R;
+                end
+            end
+
+            R: begin
+                if(bus.rvalid == 1'b1 && finish_arrange) begin
+                    bus.rready = 1'b1;
+                    if(decode_addr_ready && read) begin
+                        next = AR;
+                    end
+                    else if(decode_addr_ready && write) begin
+                        next = AW;
+                    end
+                    else begin
+                        next = IDLE;
+                    end
+                end
+            end
+
+            AW: begin
+                bus.awvalid = 1'b1;
+                if(decode_data_ready) begin
+                    bus.wvalid = 1'b1;
+                end
+                if(bus.awready == 1'b1) begin
+                    if(bus.wready == 1'b1) begin
+                        next = B;
+                    end
+                    else begin
+                        next = W;
+                    end
+                end
+            end
+
+            W: begin
+                bus.wvalid = 1'b1;
+                if(bus.wready == 1'b1) begin
+                    next = B;
+                end
+            end
+
+            B: begin
+                if(bus.bvalid == 1'b1 && finish_arrange) begin
+                    bus.bready = 1'b1;
+                    if(decode_addr_ready && read) begin
+                        next = AR;
+                    end
+                    else if(decode_addr_ready && write) begin
+                        next = AW;
+                    end
+                    else begin
+                        next = IDLE;
+                    end
+                end
+            end
+
+        endcase
+    end
+/*
+    modport master (
+        output araddr, arvalid,
+        input  arready,
+
+        input  rdata, rvalid,       rresp
+        output rready,
+
+        output awaddr, awvalid,
+        input  awready,
+
+        output wdata, wvalid,       wstrb
+        input  wready,
+
+        input  bvalid,              bresp
+        output bready
+    );
+*/
+
+    
+endmodule
+
+
+
+
+
+
+

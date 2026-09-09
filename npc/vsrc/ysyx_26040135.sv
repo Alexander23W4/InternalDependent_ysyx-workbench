e9e2dd6e1bc0ee8de5970e9a37938579e0133bf0
UB22.04Wang <2842wang6843w@gmail.com>
2026-09-09 16:48:06 +0800

    Wed Sep  9 16:48:06 CST 2026

--- npc/vsrc/ysyx_26040135.sv
+++ npc/vsrc/ysyx_26040135.sv
@@ -167,8 +167,6 @@ module ysyx_26040135(
     // logic [1:0] __ifu_error, __lsu_error,
     // logic __ifu_master_validation_error,
 
-    `include "vsrc/dpi-f.sv"
-
     reg [31:0] pc;   // reg
     assign _pc = pc;   // ⭐⭐ 引出C
 

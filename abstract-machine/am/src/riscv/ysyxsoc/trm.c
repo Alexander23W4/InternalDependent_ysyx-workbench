51470e14c8921a0146f92123a21703332f78511e
UB22.04Wang <2842wang6843w@gmail.com>
2026-09-14 13:06:42 +0800

    Mon Sep 14 13:06:42 CST 2026

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -41,7 +41,7 @@ extern char _pmem_start;
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
-static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
+static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS 
 
 void putch(char ch) {
   *(volatile uint8_t  *)YSYXSOC_SERIAL_ADDR = ch;
@@ -56,3 +56,5 @@ void _trm_init() {
   int ret = main(mainargs);
   halt(ret);
 }
+
+

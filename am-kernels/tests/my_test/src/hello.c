0e0770a07828675fbaf4d3f8d54cd74c78e02771
UB22.04Wang <2842wang6843w@gmail.com>
2026-09-15 11:30:19 +0800

    Tue Sep 15 11:30:19 CST 2026 working on uart

---
+++ am-kernels/tests/my_test/src/hello.c
@@ -0,0 +1,14 @@
+#include <amtest.h>
+
+void hello() {
+  for (int i = 0; i < 10; i ++) {
+    putstr("Hello, AM World @ " __ISA__ "\n");
+  }
+}
+
+int main(){
+  putch('A');
+  hello();
+  return 0;
+}
+

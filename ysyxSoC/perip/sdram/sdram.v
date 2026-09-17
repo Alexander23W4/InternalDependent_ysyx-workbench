7fb920e913d04836298a51693586f67db87fac6d
UB22.04Wang <2842wang6843w@gmail.com>
2026-09-17 11:11:18 +0800

    Thu Sep 17 11:11:18 CST 2026

--- ysyxSoC/perip/sdram/sdram.v
+++ ysyxSoC/perip/sdram/sdram.v
@@ -29,21 +29,41 @@ CAS: 接收到READ命令, 把数据放到DQ总线上, 之间的时间延迟 dela
 你需要实现SDRAM控制器会发送的命令, 其中PRECHARGE和AUTO REFRESH命令与存储单元的电气特性相关, 在仿真环境中不必考虑, 因此可以将其实现成NOP. 
 此外, Mode寄存器只需要实现CAS Latency 和 Burst Length, 其他字段可忽略.
 
+// ⭐: SDRAM总容量: 8192 * 512 * 4 * 2(Byte) = 2^25 Bytes = 32 MB
 */
 
 module sdram(
   input        clk,
   input        cke,
+
   input        cs,
   input        ras,
   input        cas,
   input        we,
+
   input [12:0] a,
   input [ 1:0] ba,
   input [ 1:0] dqm,
   inout [15:0] dq
 );
+  reg [15:0] memory [0:3][0:8191][0:511];  // ⭐
 
-  assign dq = 16'bz;
+
+  localparam IDLE = 0, ACTIVATE = 1, MODE = 2, READ = 3, WRITE = 4;
+  reg [2:0] state, next;
+
+  wire [3:0] ctrl;
+  assign ctrl = {cs, ras, cas, we};
+
+  always @(*) begin
+    next = state;
+
+    case (state)
+      IDLE: begin
+        
+      end
+    endcase
+    
+  end
 
 endmodule

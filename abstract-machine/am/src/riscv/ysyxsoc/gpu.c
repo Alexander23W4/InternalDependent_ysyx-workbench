4d33bc7bdf0f6d737a3d353bec3405063c195bab
UB22.04Wang <2842wang6843w@gmail.com>
2026-09-18 09:59:42 +0800

    Fri Sep 18 09:59:42 CST 2026

--- abstract-machine/am/src/riscv/ysyxsoc/gpu.c
+++ abstract-machine/am/src/riscv/ysyxsoc/gpu.c
@@ -1,5 +1,18 @@
 #include <am.h>
 #include "ysyxsoc.h"
+/*
+NVBoard提供的VGA屏幕分辨率是640x480
+
+VGA	   0x2100_0000~0x211f_ffff
+这段地址空间其实是帧缓冲, 程序往其中写入像素信息, 即可在输出到NVBoard的VGA区域.
+
+ysyxSoC/perip/vga/vga_top_apb.v   VGA控制器
+
+关于帧缓冲, 目前你可以暂时使用SRAM等简单存储器的方式来实现. 但需要注意, 在真实情况中, 这种实现方案的成本较高: 以上文提到的640x480分辨率为例, 如果每个像素占4字节, 将需要1.17MB的SRAM, 这将占用不少的流片面积
+接入NVBoard, 绑定相关引脚
+在AM IOE中添加代码, 将像素信息写入VGA控制器的帧缓冲中
+由于NVBoard提供的VGA机制是自动刷新的, 因此无需实现AM中的画面同步功能
+*/
 
 #define SYNC_ADDR (NPC_VGACTL_ADDR + 4)   // sync_addr
 
@@ -7,7 +20,7 @@ void __am_gpu_init() {
 
 }
 
-//  bool present, has_accel; int width, height, vmemsz
+//  ysyxsoc的硬件没有存储 VGA info的寄存器, 所以这里这个API直接返回 NVboarad 的分辨率 640*480
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
--- abstract-machine/am/src/riscv/ysyxsoc/ysyxsoc.h
+++ abstract-machine/am/src/riscv/ysyxsoc/ysyxsoc.h
@@ -15,10 +15,10 @@ SDRAM	              0xa000_0000~0xbfff_ffff
 GPIO	              0x1000_2000~0x1000_200f      GPIO 实现 IOE ?
 UART16550	          0x1000_0000~0x1000_0fff
 PS2	                  0x1001_1000~0x1001_1007      实际只需要4位, 一个字
+VGA	                  0x2100_0000~0x211f_ffff
 
 ⭐
 CLINT	              0x0200_0000~0x0200_ffff
-VGA	                  0x2100_0000~0x211f_ffff
 ChipLink MMIO	      0x4000_0000~0x7fff_ffff
 ChipLink MEM	      0xc000_0000~0xffff_ffff
 

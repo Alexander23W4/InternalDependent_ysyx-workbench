b20df65324f23a3f126453bb804f4adc1ac54a0b
UB22.04Wang <2842wang6843w@gmail.com>
2026-09-18 10:06:03 +0800

    Fri Sep 18 10:06:03 CST 2026

--- abstract-machine/am/src/riscv/ysyxsoc/gpu.c
+++ abstract-machine/am/src/riscv/ysyxsoc/gpu.c
@@ -14,7 +14,12 @@ ysyxSoC/perip/vga/vga_top_apb.v   VGA控制器
 由于NVBoard提供的VGA机制是自动刷新的, 因此无需实现AM中的画面同步功能
 
 ⭐: 
-注意, 
+review 一下, guest 是这样调用 AM 的 API 的:
+  screen_w = io_read(AM_GPU_CONFIG).width;, 这个io_read() 函数是直接返回对应的数据结构
+
+AM_DEVREG( 9, GPU_CONFIG,   RD, bool present, has_accel; int width, height, vmemsz);
+AM_DEVREG(10, GPU_STATUS,   RD, bool ready);
+AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);
 */
 
 #define SYNC_ADDR (NPC_VGACTL_ADDR + 4)   // sync_addr
@@ -23,16 +28,16 @@ void __am_gpu_init() {
 
 }
 
-//  ysyxsoc的硬件没有存储 VGA info的寄存器, 所以这里这个API直接返回 NVboarad 的分辨率 640*480
+//  ysyxsoc的硬件没有存储 VGA info的寄存器, 所以这里这个API直接返回 NVboarad 的分辨率 640*480. 为了提供统一的接口, 还是要封装一下
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = 0, .height = 0,
     .vmemsz = 0
   };
-  uint32_t vga_info = *(volatile uint32_t *)NPC_VGACTL_ADDR;
-  cfg->height = vga_info & 0xFFFF;
-  cfg->width = (vga_info >> 16) & 0xFFFF;
+  // 这里直接给 NVboard 的分辨率数据, 没有寄存器可以读取
+  cfg->height = 480;
+  cfg->width = 640;
   cfg->vmemsz = cfg->height * cfg->width * sizeof(uint32_t);
 }
 

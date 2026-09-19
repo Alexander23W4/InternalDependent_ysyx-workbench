4972619dbb94883980ef4236ae548f5ff5679830
UB22.04Wang <2842wang6843w@gmail.com>
2026-09-19 20:35:06 +0800

    Sat Sep 19 20:35:06 CST 2026

--- npc/texts/cache.md
+++ npc/texts/cache.md
@@ -26,6 +26,43 @@ cache miss
 
 ## icache
 数据块 的 tag:   addr / 4
-cache块 的 编号:   (addr / 4) % k (direct-mapped), 新块代替旧块
+cache块 的 编号(块索引)index:   (addr / 4) % k (direct-mapped), 新块代替旧块
+
+块大小是b字节, 共k个cache块, 直接映射: 
+tag = addr / b
+index = (addr / b) % k
+
+通常取b和k为2的幂, 假设b = 2^m, k = 2^n. 
+假设addr为32位, 则有tag = addr / 2^m = addr[31:m], index = (addr / 2^m) % 2^n = addr[m+n-1:m]
+记录tag时只需要记录addr[31:m+n]即可
+
+>> 一个访存地址可以划分成以下3部分: tag, index, offset. 
+1. tag部分作为数据块在cache中的唯一编号(有多少tag, 就有多少个数据块共用一个cache块)
+2. index部分作为数据块在cache中的索引
+3. offset部分属于块内偏移, 指示需要访问数据块中的哪部分数据.
+
+ 31    m+n m+n-1   m m-1    0
++---------+---------+--------+
+|   tag   |  index  | offset |
++---------+---------+--------+
+
+>> valid, tag  metadata
+为了标识一个cache块是否有效, 需要为每个cache块添加一个有效位(valid). valid和tag统称为cache块的元数据(metadata), 其含义是用于管理数据的数据, 此处被管理的数据就是cache块.
+
+>> icache工作流程
+
+IFU向icache发送取指请求
+icache获得取指请求的地址后, 根据index部分索引出一个cache块, 判断其tag与请求地址的tag是否相同, 并检查该cache块是否有效. 若同时满足上述条件, 则命中, 跳转到第5步
+通过总线在DRAM中读出请求所在的数据块
+将该数据块填入相应cache块中, 更新元数据
+向IFU返回取出的指令
+
+
+
+
+
+
+
+
 
 
---
+++ npc/vsrc/axi/axi_icache.sv
@@ -0,0 +1,103 @@
+/*
+根据上述流程, 实现一个简单的icache, 块大小为4B, 共16个cache块. 
+⭐: 本icache 并不判断地址的范围, 如果不再 SDRAM 的范围内, 那么要么漏下去交给下面报错, 要么ub
+
+⭐: 实现时, 建议将相关参数实现成可配置的, 以便于后续评估不同配置参数的性能表现. 实现后, 尝试评估其性能表现.
+
+IFU向icache发送取指请求
+
+icache获得取指请求的地址后, 根据index部分索引出一个cache块, 判断其tag与请求地址的tag是否相同, 并检查该cache块是否有效. 若同时满足上述条件, 则命中, 跳转到第5步
+
+通过总线在DRAM中读出请求所在的数据块
+
+将该数据块填入相应cache块中, 更新元数据
+
+向IFU返回取出的指令
+*/
+
+module ysyx_26040135_AXI_ICACHE (
+    ysyx_26040135_AXI4.slave bus,
+    ysyx_26040135_AXI4.master mbus,
+    input clock, 
+    input reset
+);
+    parameter CACHE_LINE_LEN = 32, CACHE_LINE_AMT = 16, INDEX_LEN = 4, OFFSET_LEN = 2, TAG_LEN = 26;
+
+
+    logic [CACHE_LINE_LEN-1:0] icache [0:CACHE_LINE_AMT-1];
+    logic [TAG_LEN-1:0]        tag    [0:CACHE_LINE_AMT-1];
+    logic                      valid  [0:CACHE_LINE_AMT-1];
+
+    logic [31:0] araddr_save;
+
+
+    logic [TAG_LEN-1:0] current_tag;
+    logic [INDEX_LEN-1:0] current_index;
+
+    assign current_tag = bus.araddr[31:INDEX_LEN+OFFSET_LEN];
+    assign current_index = bus.araddr[INDEX_LEN+OFFSET_LEN-1:OFFSET_LEN];
+
+
+    typedef enum [2:0]{ 
+        IDLE, OPERATE, DRAM, UPDATE, RETURN
+    } state_t;
+    state_t state, next;
+
+
+    always_ff @( posedge clock or posedge reset ) begin
+        if(reset) begin
+            state <= IDLE;
+            araddr_save <= '0;
+            for (int i = 0; i < CACHE_LINE_AMT; i++) begin
+                icache[i] <= '0;
+                tag[i]    <= '0;
+                valid[i]  <= 1'b0;
+            end
+        end else begin
+            state <= next;
+
+        end
+    end
+
+    always_comb begin
+        next = state;
+        bus.arready = 1'b0;
+
+        bus.rvalid  = 1'b0;
+        bus.rdata   = 32'b0;
+        bus.rresp   = 2'b00;
+        bus.rid     = 4'b0;
+        bus.rlast   = 1'b0;
+
+        bus.awready = 1'b0;
+        bus.wready  = 1'b0;
+
+        bus.bvalid  = 1'b0;
+        bus.bresp   = 2'b00;
+        bus.bid     = 4'b0;
+
+        case (state)
+            IDLE: begin
+                if(bus.arvalid) begin
+                    next = OPERATE;
+                    bus.arready = 1'b1;
+                end
+            end
+            OPERATE: begin
+                
+            end
+        endcase
+    end
+
+
+
+
+
+
+
+
+
+
+endmodule
+
+
